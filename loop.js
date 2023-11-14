c2b9d84261173a8bcd45cfb78577d74287d4ca28
La-Ruda <herempen@gmail.com>
2023-11-14 16:57:31 +0100

    bundling update

---
+++ loop.js
@@ -0,0 +1,53 @@
+const textDisplay = document.querySelector(".home__subheadline");
+
+let phrases = ['I am a Coder','and a Copywriter','also an Idea Maker'];
+
+let i = 0;
+
+let j = 0;
+
+let currentPhrase = [];
+
+let isDeleting = false;
+
+let isEnd = false;
+
+export const loop = () => {
+  
+    isEnd = false
+    textDisplay.innerHTML = currentPhrase.join('');
+  
+    if (i < phrases.length) {
+  
+      if (!isDeleting && j <= phrases[i].length) {
+        currentPhrase.push(phrases[i][j])
+        j++
+        textDisplay.innerHTML = currentPhrase.join('')
+      }
+  
+      if(isDeleting && j <= phrases[i].length) {
+        currentPhrase.pop(phrases[i][j])
+        j--
+        textDisplay.innerHTML = currentPhrase.join('')
+      }
+  
+      if (j == phrases[i].length) {
+        isEnd = true
+        isDeleting = true
+      }
+  
+      if (isDeleting && j === 0) {
+        currentPhrase = []
+        isDeleting = false
+        i++
+        if (i === phrases.length) {
+          i = 0
+        }
+      }
+    }
+
+    const spedUp = Math.random() * (50 -20) + 20;
+    const normalSpeed = Math.random() * (150 -50) + 50;
+    const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
+    setTimeout(loop, time)
+  }
--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-///////////////////// HASH CHANGE
-
-window.addEventListener("hashchange", function() {
-  console.log(hash);
-});
-
-var hash = new URL(document.URL).hash;
-
 
 /////////////////////////////////// HAMBURGER MENU 
 
@@ -53,59 +45,7 @@ const mobileHeader = document.querySelector(".header--mobile");
 
 //////////////////////////////////////// HOMESCREEN WRITINGS
 
-const textDisplay = document.querySelector(".home__subheadline");
-
-let phrases = ['I am a Coder','and a Copywriter','also an Idea Maker'];
-
-let i = 0;
-
-let j = 0;
-
-let currentPhrase = [];
-
-let isDeleting = false;
-
-let isEnd = false;
-
-const loop = () => {
-  
-    isEnd = false
-    textDisplay.innerHTML = currentPhrase.join('');
-  
-    if (i < phrases.length) {
-  
-      if (!isDeleting && j <= phrases[i].length) {
-        currentPhrase.push(phrases[i][j])
-        j++
-        textDisplay.innerHTML = currentPhrase.join('')
-      }
-  
-      if(isDeleting && j <= phrases[i].length) {
-        currentPhrase.pop(phrases[i][j])
-        j--
-        textDisplay.innerHTML = currentPhrase.join('')
-      }
-  
-      if (j == phrases[i].length) {
-        isEnd = true
-        isDeleting = true
-      }
-  
-      if (isDeleting && j === 0) {
-        currentPhrase = []
-        isDeleting = false
-        i++
-        if (i === phrases.length) {
-          i = 0
-        }
-      }
-    }
-
-    const spedUp = Math.random() * (50 -20) + 20;
-    const normalSpeed = Math.random() * (150 -50) + 50;
-    const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
-    setTimeout(loop, time)
-  }
+import { loop } from "./loop";
 
 loop();
 
